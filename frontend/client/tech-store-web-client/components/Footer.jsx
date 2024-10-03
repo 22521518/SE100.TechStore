29b55777ab50eac2bee7f70d8b71d01642a0783f
Xuana252 <notfuny4927@gmail.com>
2024-10-03 13:46:49 +0700

    client init

---
+++ frontend/client/tech-store-web-client/components/Footer.jsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="h-fit w-full bg-black">
+      <div>
+        <button>hello</button>
+      </div>
+      <div>
+        <button>hello</button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
