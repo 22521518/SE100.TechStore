6cc2fbfcf916e53ecf61e55173b66c361b655c33
Xuana252 <notfuny4927@gmail.com>
2024-11-12 22:48:08 +0700

    refine UIs mobile

--- frontend/mobile/app/src/main/java/com/example/electrohive/MainActivity.java
+++ frontend/mobile/app/src/main/java/com/example/electrohive/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment);
+        // Make the status bar transparent
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(android.R.color.transparent));
+        }
     }
 }
