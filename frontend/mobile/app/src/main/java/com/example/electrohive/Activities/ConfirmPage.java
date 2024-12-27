3eb29672a2f80e65e7f1705f31bf3afe7238d28a
KeriaKH <151910742+KeriaKH@users.noreply.github.com>
2024-12-27 10:31:31 +0700

    change activity

--- frontend/mobile/app/src/main/java/com/example/electrohive/Activities/ConfirmPage.java
+++ frontend/mobile/app/src/main/java/com/example/electrohive/Activities/ConfirmPage.java
@@ -41,5 +41,179 @@ import vn.zalopay.sdk.listeners.PayOrderListener;
 
 public class ConfirmPage  extends AppCompatActivity {
 
+    TextView date,payment,address,Subtotal,Discount,ShipCost,GrandTotal,confirm,voucher_code,voucher_discount_percentage;
+
+
+    OrderViewModel orderViewModel;
+    CartViewModel cartViewModel= CartViewModel.getInstance();
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void  onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirm_page);
+
+        orderViewModel = new OrderViewModel(this);
+
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        ZaloPaySDK.init(554, Environment.SANDBOX);
+
+
+
+        ImageButton backbutton=findViewById(R.id.backbutton);
+        backbutton.setOnClickListener(v -> {
+            finish();
+        });
+        String payment_method= getIntent().getStringExtra("payment_method");
+        payment=findViewById(R.id.payment_method);
+        payment.setText(payment_method);
+
+        date=findViewById(R.id.date);
+        createDate();
+        CheckoutAddress Address=(CheckoutAddress) getIntent().getSerializableExtra("address");
+
+        String formattedDate = String.format("%s, %s %s, %s", Address.getAddress(),Address.getWard(),Address.getDistrict(),Address.getCity());
+        address=findViewById(R.id.address);
+        address.setText(formattedDate);
+
+        String productJson = getIntent().getStringExtra("checkedItems");
+        Gson gson = new Gson();
+        ArrayList<CartItem> cartItems=gson.fromJson(productJson, new TypeToken<ArrayList<CartItem>>() {}.getType());
+        RecyclerView recyclerView = findViewById(R.id.rc_trend);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        PaymentAdapter paymentAdapter=new PaymentAdapter(this,cartItems);
+        recyclerView.setAdapter(paymentAdapter);
+        ArrayList<OrderItemRequest> list =new ArrayList<>();
+        for(CartItem item: cartItems)
+        {
+            list.add(new OrderItemRequest(item.getProductId(), item.getQuantity(),item.getProduct().getPrice(),item.getTotalPrice()));
+        }
+
+        Bundle bundle = getIntent().getExtras();
+        Voucher voucher = (Voucher) bundle.getSerializable("voucher");
+        String subtotal= bundle.getString("subtotal");
+        String discount=bundle.getString("discount");
+        String shipCost=bundle.getString("shipCost");
+        String grandTotal=bundle.getString("grandTotal");
+        voucher_code = findViewById(R.id.receipt_discount_code);
+        voucher_discount_percentage = findViewById(R.id.receipt_discount_percentage);
+        Subtotal=findViewById(R.id.receipt_subtotal);
+        Discount=findViewById(R.id.receipt_discount);
+        ShipCost=findViewById(R.id.receipt_shipment_cost);
+        GrandTotal=findViewById(R.id.receipt_grand_total);
+        Subtotal.setText(subtotal);
+        Discount.setText(discount);
+        ShipCost.setText(shipCost);
+        GrandTotal.setText(grandTotal);
+        if(voucher!=null) {
+            voucher_code.setText(voucher.getVoucherName());
+            voucher_discount_percentage.setText("("+voucher.getDiscountAmount()+"%)");
+        }
+
+        confirm=findViewById(R.id.confirm);
+        confirm.setOnClickListener(v -> {
+            String numericPart = GrandTotal.getText().toString()
+                    .replace(" VNĐ", "")
+                    .replace(",", "")
+                    .trim();
+            double price = Double.parseDouble(numericPart);
+            if(payment_method.equals("COD"))
+            {
+                orderViewModel.postUserOrder(price,list,PAYMENT_METHOD.valueOf(payment_method),Address,voucher).observe(this, res-> {
+                    if(res.isSuccess()) {
+                        Intent intent1=new Intent(getApplicationContext(),ReceiptPage.class);
+                        intent1.putExtra("checkedItems",productJson);
+                        intent1.putExtra("address", Address);
+                        intent1.putExtra("voucher",voucher);
+                        intent1.putExtra("payment_method",getIntent().getStringExtra("payment_method"));
+                        intent1.putExtra("date",date.getText().toString());
+                        intent1.putExtras(bundle);
+                        for(CartItem item: cartItems){
+                            cartViewModel.deleteCartItem(item.getProductId());
+                        }
+                        startActivity(intent1);
+                    } else {
+                        Toast.makeText(ConfirmPage.this, res.getMessage(), Toast.LENGTH_SHORT).show();
+                    }});
+            }
+            else if(payment_method.equals("ZALOPAY"))
+            {
+                CreateOrder orderApi = new CreateOrder();
+                try {
+                    JSONObject data = orderApi.createOrder(numericPart);
+                    String code = data.getString("returncode");
+                    if (code.equals("1")) {
+                        String token = data.getString("zptranstoken");
+                        ZaloPaySDK.getInstance().payOrder(ConfirmPage.this, token, "demozpdk://app", new PayOrderListener() {
+                            @Override
+                            public void onPaymentSucceeded(String s, String s1, String s2) {
+                                orderViewModel.postUserOrder(price,list, PAYMENT_METHOD.valueOf(payment_method),Address,voucher).observeForever(res-> {
+                                    if(res.isSuccess()) {
+                                        Intent intent1=new Intent(getApplicationContext(),ReceiptPage.class);
+                                        intent1.putExtra("checkedItems",productJson);
+                                        intent1.putExtra("address", Address);
+                                        intent1.putExtra("voucher",voucher);
+                                        intent1.putExtra("payment_method",getIntent().getStringExtra("payment_method"));
+                                        intent1.putExtra("date",date.getText().toString());
+                                        intent1.putExtras(bundle);
+                                        for(CartItem item: cartItems){
+                                            cartViewModel.deleteCartItem(item.getProductId());
+                                        }
+                                        startActivity(intent1);
+                                    } else {
+                                        Toast.makeText(ConfirmPage.this, res.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                            }
+
+                            @Override
+                            public void onPaymentCanceled(String s, String s1) {
+                            }
+
+                            @Override
+                            public void onPaymentError(ZaloPayError zaloPayError, String s, String s1) {
+                            }
+                        });
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+    }
+    @Override
+    protected void onNewIntent(@NonNull Intent intent) {
+        super.onNewIntent(intent);
+        ZaloPaySDK.getInstance().onResult(intent);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void createDate()
+    {
+        LocalDate currentDate = LocalDate.now();
+
+        // Lấy tên thứ (Monday, Tuesday, ...)
+        String dayOfWeek = currentDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+
+        // Lấy tháng (October, November, ...)
+        String month = currentDate.getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+
+        // Lấy ngày (9, 10, ...)
+        int day = currentDate.getDayOfMonth();
+
+        // Lấy năm (2024, ...)
+        int year = currentDate.getYear();
+
+        // Tạo chuỗi theo định dạng
+        String formattedDate = String.format("%s, %s %d, %d", dayOfWeek, month, day, year);
+        date.setText(formattedDate);
+    }
+
 
 }
--- frontend/mobile/app/src/main/java/com/example/electrohive/Activities/ProductDetailPage.java
+++ frontend/mobile/app/src/main/java/com/example/electrohive/Activities/ProductDetailPage.java
@@ -47,5 +47,301 @@ import me.relex.circleindicator.CircleIndicator3;
 
 public class ProductDetailPage extends DrawerBasePage {
 
+    private ProgressBar loadingSpinner;
+
+    private ProductViewModel productViewModel;
+
+    private CartViewModel cartViewModel;
+
+    private RatingBar product_average_rating;
+    private TextView product_name;
+    private TextView product_feedback_count_1;
+    private TextView product_price;
+    private TextView product_original_price;
+    private TextView product_discount;
+    private TextView product_stock_count;
+    private TextView product_add_to_cart_button;
+    private TextView product_buy_now_button;
+    private TextView product_detail;
+    private TextView product_rating;
+    private TextView product_5_star_count;
+    private TextView product_4_star_count;
+    private TextView product_3_star_count;
+    private TextView product_2_star_count;
+    private TextView product_1_star_count;
+    private TextView product_0_star_count;
+    private TextView see_feedback_button;
+
+    private EditText product_quantity_input;
+    private RecyclerView product_attribute_listview;
+    private RecyclerView product_listview;
+
+    private  ViewPager2 imageSlider;
+    private CircleIndicator3 indicator ;
+
+
+
+    private List<Product> productList = new ArrayList<>();
+    private List<ProductImage> productImages = new ArrayList<>(3);
+
+    private List<ProductAttribute> productAttributes = new ArrayList<>();
+
+    private ProductImagesAdapter imagesAdapter;
+
+    private ProductAttributeAdapter attributeAdapter;
+
+    private ProductAdapter productAdapter;
+
+    private Product curProduct = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContent(R.layout.product_detail);
+
+
+
+        loadingSpinner = findViewById(R.id.loading_spinner);
+        loadingSpinner.setVisibility(View.VISIBLE);
+
+        imageSlider = findViewById(R.id.dt_display_image);
+        indicator  = findViewById(R.id.dt_display_image_indicator );
+
+        product_average_rating = findViewById(R.id.product_average_rating);
+        product_name = findViewById(R.id.dt_product_name);
+        product_feedback_count_1 = findViewById(R.id.product_feedback_count_1);
+        product_price = findViewById(R.id.dt_product_price);
+        product_original_price = findViewById(R.id.dt_product_original_price);
+        product_discount = findViewById(R.id.dt_product_discount);
+        product_stock_count = findViewById(R.id.product_stock_count);
+        product_add_to_cart_button = findViewById(R.id.product_add_to_cart_button);
+        product_buy_now_button = findViewById(R.id.product_buy_now_button);
+        product_detail = findViewById(R.id.product_detail);
+        product_rating = findViewById(R.id.product_rating);
+        product_5_star_count = findViewById(R.id.product_5_star_count);
+        product_4_star_count = findViewById(R.id.product_4_star_count);
+        product_3_star_count = findViewById(R.id.product_3_star_count);
+        product_2_star_count = findViewById(R.id.product_2_star_count);
+        product_1_star_count = findViewById(R.id.product_1_star_count);
+        product_0_star_count = findViewById(R.id.product_0_star_count);
+        see_feedback_button = findViewById(R.id.see_feedback_button);
+        product_quantity_input = findViewById(R.id.product_quantity_input);
+        product_attribute_listview = findViewById(R.id.product_attribute_listview);
+
+        product_listview = findViewById(R.id.product_listview);
+        ImageButton backbutton=findViewById(R.id.backbutton);
+        backbutton.setOnClickListener(v->{
+            finish();
+        });
+        Intent intent = getIntent();
+        String productId = intent.getStringExtra("PRODUCT_ID");
+        productViewModel = new ProductViewModel();
+        cartViewModel = CartViewModel.getInstance();
+
+        productAdapter = new ProductAdapter(
+                ProductDetailPage.this,
+                productList,
+                product -> showProductDetail(product.getProductId())
+        );
+        product_listview.setLayoutManager(new GridLayoutManager(ProductDetailPage.this, 2));
+        product_listview.setAdapter(productAdapter);
+
+        attributeAdapter = new ProductAttributeAdapter(
+                ProductDetailPage.this,
+                productAttributes
+        );
+        product_attribute_listview.setLayoutManager(new LinearLayoutManager(ProductDetailPage.this));
+        product_attribute_listview.setAdapter(attributeAdapter);
+
+        imagesAdapter = new ProductImagesAdapter(
+                ProductDetailPage.this,
+                productImages
+        );
+        imageSlider.setAdapter(imagesAdapter);
+        indicator.setViewPager(imageSlider);
+
+        product_quantity_input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // No action needed here
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // No action needed here
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // If the input is empty, reset it to "1"
+                if (s.toString().isEmpty()) {
+                    product_quantity_input.setText("1");
+                    product_quantity_input.setSelection(product_quantity_input.getText().length()); // Move cursor to the end
+                }
+            }
+        });
+
+        productViewModel.getProducts(16).observe(this, apiResponse -> {
+            // Hide the loading spinner once the response is processed
+            loadingSpinner.setVisibility(View.GONE);
+
+            // Check if the response is not null and was successful
+            if (apiResponse != null && apiResponse.isSuccess()) {
+                List<Product> products = apiResponse.getData();
+                if (products != null) {
+                    productAdapter.updateProducts(products);  // Method to update the adapter data
+                }
+            } else {
+                // Handle failure scenario
+                String errorMessage = apiResponse != null ? apiResponse.getMessage() : "Failed to fetch products. Please try again.";
+                Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        productViewModel.getProductDetail(productId).observe(this, apiResponse -> {
+
+            // Check if the response is not null and was successful
+            if (apiResponse != null && apiResponse.isSuccess()) {
+                Product product = apiResponse.getData();
+                if (product != null) {
+                    // Handle the successful response by updating the UI
+                    curProduct = product;
+                    productImages = product.getImages();
+                    imagesAdapter.updateImages(productImages);
+                    indicator.setViewPager(imageSlider);
+
+                    product_name.setText(product.getProductName());
+                    product_feedback_count_1.setText(product.getProductFeedbacks().size() + " reviews");
+                    product_average_rating.setRating(calculateAverageRating(product.getProductFeedbacks()));
+                    product_price.setText(Format.getFormattedTotalPrice(product.getRetailPrice()) + " VNĐ");
+                    product_original_price.setText(Format.getFormattedTotalPrice(product.getPrice()) + "  VNĐ");
+                    product_discount.setText("-" + product.getDiscount() + "%");
+                    product_stock_count.setText(product.getStockQuantity() + " in-stock");
+                    product_add_to_cart_button.setOnClickListener(v -> addItemToCart(productId, Integer.parseInt(product_quantity_input.getText().toString())));
+                    product_buy_now_button.setOnClickListener(v -> buyProduct(productId,curProduct, Integer.parseInt(product_quantity_input.getText().toString())));
+                    product_detail.setText(product.getDescription());
+                    product_rating.setText(calculateAverageRating(product.getProductFeedbacks()) + " / 5");
+                    product_5_star_count.setText("★★★★★ (" + countRating(5, product.getProductFeedbacks()) + ")");
+                    product_4_star_count.setText("★★★★ (" + countRating(4, product.getProductFeedbacks()) + ")");
+                    product_3_star_count.setText("★★★ (" + countRating(3, product.getProductFeedbacks()) + ")");
+                    product_2_star_count.setText("★★ (" + countRating(2, product.getProductFeedbacks()) + ")");
+                    product_1_star_count.setText("★ (" + countRating(1, product.getProductFeedbacks()) + ")");
+                    product_0_star_count.setText("(" + countRating(0, product.getProductFeedbacks()) + ")");
+                    see_feedback_button.setOnClickListener(v -> seeFeedback(productId));
+                    Log.d("ProductAttributes", product.getAttributes().toString());
+
+                    // Update attributes adapter
+                    attributeAdapter.updateAttribute(product.getAttributes());
+                }
+            } else {
+                // Handle failure scenario (optional)
+                String errorMessage = apiResponse != null ? apiResponse.getMessage() : "Failed to fetch product details.";
+                Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+    private boolean addItemToCart(String productId, int quantity) {
+        if (quantity <= 0) {
+            Toast.makeText(this, "Please enter a valid quantity", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (curProduct!=null && quantity > curProduct.getStockQuantity()) {
+            Toast.makeText(this, "Insufficient quantity! please try again", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        cartViewModel.addItemToCart(productId, quantity).observe(this, apiResponse -> {
+            if (apiResponse != null && apiResponse.isSuccess()) {
+                // Check if the response is successful
+                Boolean isAdded = apiResponse.getData();
+                if (isAdded != null && isAdded) {
+                    Toast.makeText(getApplicationContext(), "Product added to cart", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to add product to cart", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                // Handle failure or error response
+                String errorMessage = apiResponse != null ? apiResponse.getMessage() : "An error occurred";
+                Toast.makeText(getApplicationContext(), errorMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return true;
+
+    }
+
+    private void buyProduct(String productId,Product product, int quantity) {
+//        boolean isAddedToCart = addItemToCart(productId, quantity);
+//
+//        // Proceed to Cart page if the item was added to cart
+//        if (isAddedToCart) {
+//            Intent intent = new Intent(ProductDetailPage.this, CartPage.class);
+//            startActivity(intent);
+//        }
+        ArrayList<CartItem> items=new ArrayList<>();
+        CartItem item=new CartItem(PreferencesHelper.getCustomerData().getCustomerId(),productId,quantity,product);
+        items.add(item);
+        System.out.println(items);
+        Gson gson = new Gson();
+        String json = gson.toJson(items);
+        Intent intent=new Intent(getApplicationContext(),CheckoutPage.class);
+        intent.putExtra("checkedItems",json);
+        startActivity(intent);
+    }
+
+
+    private String countRating(int i, List<ProductFeedback> list) {
+        if (list == null || list.isEmpty()) {
+            return "0";  // Return "0" if the list is empty or null
+        }
+
+        int count = 0;
+        for (ProductFeedback fb : list) {
+            if (fb.getRating() == i) {
+                count++;
+            }
+        }
+
+
+        // Return the average rating as a String
+        return Integer.toString(count);  // Returns the rating rounded to one decimal place
+    }
+
+
+    private float calculateAverageRating(List<ProductFeedback> list) {
+        if (list == null || list.isEmpty()) {
+            return 0;  // Return "0" if the list is empty or null
+        }
+
+        float rating = 0;
+        for (ProductFeedback fb : list) {
+            rating += fb.getRating();
+        }
+
+        // Calculate the average rating
+        rating /= list.size();
+
+        // Round to 1 decimal place
+        return Math.round(rating * 10) / 10.0f;
+    }
+
+
+
+
+    private void seeFeedback(String productId) {
+        Intent intent = new Intent(ProductDetailPage.this, ProductFeedbackPage.class);
+        intent.putExtra("PRODUCT_ID", productId);
+
+        startActivity(intent);
+    }
+
+    private void showProductDetail(String productId) {
+        Intent intent = new Intent(ProductDetailPage.this, ProductDetailPage.class);
+        intent.putExtra("PRODUCT_ID", productId);
+        startActivity(intent);
+    }
 
 }
