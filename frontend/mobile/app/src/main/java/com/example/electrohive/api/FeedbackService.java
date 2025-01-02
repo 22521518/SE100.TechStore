6404442b5269dd388cd0b4a579715943b6c9e474
KeriaKH <151910742+KeriaKH@users.noreply.github.com>
2025-01-02 22:44:44 +0700

    api rating

--- frontend/mobile/app/src/main/java/com/example/electrohive/api/FeedbackService.java
+++ frontend/mobile/app/src/main/java/com/example/electrohive/api/FeedbackService.java
@@ -11,12 +11,14 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface FeedbackService {
+    //post rating
     @POST("/feedback/{id}")
     Call<JsonObject> addFeedback(
             @Header("Content-Type") String header,
             @Path("id") String productId,
                                  @Body JsonObject feedback);
 
+    //get rating
     @GET("/feedback/{id}")
     Call<JsonArray> getFeedbacks(@Path("id") String productId);
 }
